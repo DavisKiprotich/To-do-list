5ade0eaf1137305b67d98cb1395e90e7ca76c1cb
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-12 11:06:51 +0300

    Created Button file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PageTitle from './components/PageTitle';
+import AppHeader from './components/AppHeader';
+import style from './styles/modules/app.module.scss';
 
 import './App.css';
 
@@ -7,6 +9,9 @@ function App() {
   return (
     <div className="container">
     <PageTitle>TODO LIST</PageTitle>
+    <div className={style.app__wrapper}>
+    <AppHeader />
+    </div>
     </div>
   );
 }
---
+++ src/components/AppHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+
+function AppHeader() {
+  return (
+    <div>
+    <h1>This is the header</h1>
+    <Button>Click Me</Button>
+    </div>
+    
+  )
+}
+
+export default AppHeader
