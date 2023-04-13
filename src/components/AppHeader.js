9cc980dba12a9d0f6433663105784b1e1c092004
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-13 16:06:56 +0300

    Button changes

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Button from './Button';
 
 function AppHeader() {
   return (
     <div>
     <h1>This is the header</h1>
+    <Button>Click Me</Button>
     </div>
     
   )
---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from '../styles/modules/button.module.scss'
+
+function Button({children}) {
+  return (
+    <button className={style.button} type="button">{ children }</button>
+  )
+}
+
+export default Button
