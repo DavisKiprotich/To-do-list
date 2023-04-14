c7f205570e86212634ab76c531aa5c4b78787567
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-14 16:50:38 +0300

    today's commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
-import React , { Children }from 'react';
+// eslint-disable-next-line
+import React , { children }from 'react';
 import styles from '../styles/modules/button.module.scss';
-import { getClasses } from '../utilities/getClasses';
+import { getClasses } from '../utilities/getClasses'
 
 
 const buttonTypes = {
@@ -8,9 +9,17 @@ const buttonTypes = {
     secondary: 'secondary',
 };
 
-function Button({ children, variant = 'primary' }) {
+function Button({ children, type ,variant, ...rest }) {
   return (
-    <button className={getClasses([styles.button], styles[`button--${buttonTypes[variant]}`])} type="button">{ children }</button>
+    <button className={getClasses({ [styles.button, styles[`button--${buttonTypes[variant]}`]
+  ] })} 
+    type={type === 'submit' ? 'submit' : 'button'} {...rest}>{ children }</button>
+  )
+}
+
+function SelectButton({ children, ...rest }){
+  return (
+    <select className={getClasses({[styles.button, styles.button__select]})} {...rest}>{children}</select>
   )
 }
 
