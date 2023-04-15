fcf0f7d4f2235b01d746ec66f88c67ca17dc82b1
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-15 21:57:16 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PageTitle from './components/PageTitle';
-import style from './styles/modules/app.module.scss';
+import styles from './styles/modules/app.module.scss';
 import AppHeader from './components/AppHeader';
 
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="container">
     <PageTitle>TODO LIST</PageTitle>
-    <div className={style.app__wrapper}><AppHeader /></div>
+    <div className={styles.app__wrapper}><AppHeader /></div>
     </div>
   );
 }
