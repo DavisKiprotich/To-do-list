ba4ff2e491ad2de6acb1f7a49bdae8b3b044bb37
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-15 08:41:47 +0300

    Module inheritance

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Button from './Button'
+import Button, {SelectButton} from './Button';
+import styles from '../styles/modules/app.module.scss';
+
 
 function AppHeader() {
   return (
-    <div>
-    <h1>This is the header</h1>
+    <div className={styles.appHeader}>
     <Button variant='primary'>Click Me</Button>
+    <SelectButton id='status'>
+      <option value='all'>ALL</option>
+      <option value='incomplete'>Incomplete</option>
+      <option value='complete'>Complete</option>
+    </SelectButton>
     </div>
     
   )
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,32 @@
-// eslint-disable-next-line
-import React , { children }from 'react';
+import React from 'react';
 import styles from '../styles/modules/button.module.scss';
-import { getClasses } from '../utilities/getClasses'
-
+import { getClasses } from '../utilities/getClasses';
 
 const buttonTypes = {
-    primary: 'primary',
-    secondary: 'secondary',
+  primary: 'primary',
+  secondary: 'secondary',
 };
 
-function Button({ children, type ,variant, ...rest }) {
+function Button({ type, variant = 'primary', children, ...rest }) {
   return (
-    <button className={getClasses({ [styles.button, styles[`button--${buttonTypes[variant]}`]
-  ] })} 
-    type={type === 'submit' ? 'submit' : 'button'} {...rest}>{ children }</button>
-  )
+    <button
+      type={type === 'submit' ? 'submit' : 'button'}
+      className={getClasses([
+        styles.button,
+        styles[`button--${buttonTypes[variant]}`],
+      ])}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
 
 function SelectButton({ children, ...rest }){
-  return (
-    <select className={getClasses({[styles.button, styles.button__select]})} {...rest}>{children}</select>
+  return(
+    <select className={getClasses([styles.button, styles.button__select])} {...rest}>{children}</select>
   )
 }
 
+export { SelectButton };
 export default Button
