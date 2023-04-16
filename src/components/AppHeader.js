2aa06de47b94a2ead099c5b98d07f7c626bfcb5b
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-16 21:56:20 +0300

    another one

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button, {SelectButton} from './Button';
 import styles from '../styles/modules/app.module.scss';
 import Todomodel from './Todomodel';
 
 function AppHeader() {
+  const { modelOpen, setModelOpen } = useState(true);
   return (
     <div className={styles.appHeader}>
-    <Button variant='primary'>Add Task</Button>
+    <Button variant='primary' onClick={() => setModelOpen(true)}>Add Task</Button>
     <SelectButton id='status'>
       <option value='all'>ALL</option>
       <option value='incomplete'>Incomplete</option>
       <option value='complete'>Complete</option>
     </SelectButton>
-    <Todomodel/>
+    <Todomodel modelOpen={modelOpen} setModelOpen={setModelOpen} />
     </div>
     
   )
--- src/components/Todomodel.js
+++ src/components/Todomodel.js
@@ -1,27 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/modules/modal.module.scss';
 import { MdOutlineClose } from 'react-icons/md'
+import Button from './Button';
 
-function Todomodel() {
+function Todomodel({ modelOpen, setModelOpen }) {
+    const [title, setTitle] = useState('');
+    let [description, setDescription] = useState('');
+    let [status, setStatus] = useState('open');
+    let [deadline, setDeadline] = useState('');
+    let [tag, setTag] = useState('');
   return (
-    <div className={styles.wrapper}>
-        <div className={styles.container}>
-            <div className={styles.closeButton}>
-                <MdOutlineClose/>
+
+    modelOpen && (
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <div className={styles.closeButton}
+                onClick={() => setModelOpen(false)}
+                onKeyDown={() => setModelOpen(false)}
+                tabIndex={0}
+                role='button'
+                >
+                    <MdOutlineClose/>
+                </div>
+                <form className={styles.form}>
+                    <h1 className={styles.formTitle}>Add Task</h1>
+                    <label htmlFor='title'>
+                        Title
+                        <input type='text' id='title' value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        maxLength='100' />
+                    </label>   
+                    <label htmlFor='description'>
+                        Description
+                        <input type='text' id='description' value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        maxLength='1000' />
+                    </label>   
+                    <label htmlFor='tag'>
+                        Tags
+                        <input type='text' id='tag' value={tag}
+                        onChange={(e) => setTag(e.target.value)} />
+                    </label>   
+                    <label htmlFor='deadline'>
+                        DEADLINE
+                        <input type='date' id='deadline' value={deadline}
+                        onChange={(e) => setDeadline(e.target.value)}
+                         />
+                    </label>   
+                    <label htmlFor='status'>
+                        Status
+                        <select type='status' id='status' value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                        >
+                            <option value='open'>OPEN</option>
+                            <option value='working'>WORKING</option>
+                            <option value='done'>DONE</option>
+                            <option value='overdue'>OVERDUE</option>
+                        </select>
+                    </label>  
+                    <div className={styles.buttonContainer}>
+                        <Button type='submit' variant='primary'>Add Task</Button>
+                        <Button type='button' variant='secondary' 
+                        onClick={() => setModelOpen(false)}
+                        onKeyDown={() => setModelOpen(false)}
+                        >Cancel</Button>
+                    </div> 
+                </form>
             </div>
-            <form className={styles.form}>
-                <h1 className={styles.formTitle}>Add Task</h1>
-                <label htmlFor='title'>
-                    Title
-                    <input type='text' id='title' />
-                </label>   
-                <label htmlFor='status'>
-                    Status
-                    <input type='status' id='status' />
-                </label>   
-            </form>
         </div>
-    </div>
+    )    
   )
 }
 
