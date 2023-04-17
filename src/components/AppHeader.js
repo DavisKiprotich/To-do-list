cdf5432b0bcac08e84ae8d5a3008583e061986f4
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-17 08:15:26 +0300

    change slight

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button, {SelectButton} from './Button';
+import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
 import Todomodel from './Todomodel';
 
@@ -9,9 +9,10 @@ function AppHeader() {
     <div className={styles.appHeader}>
     <Button variant='primary' onClick={() => setModelOpen(true)}>Add Task</Button>
     <SelectButton id='status'>
-      <option value='all'>ALL</option>
-      <option value='incomplete'>Incomplete</option>
-      <option value='complete'>Complete</option>
+      <option value='open'>OPEN</option>
+      <option value='working'>WORKING</option>
+      <option value='done'>DONE</option>
+      <option value='overdue'>OVERDUE</option>
     </SelectButton>
     <Todomodel modelOpen={modelOpen} setModelOpen={setModelOpen} />
     </div>
