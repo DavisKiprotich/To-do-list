ccc855f22106902dafb6ec81e15f7194e4230bf3
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-24 07:49:05 +0300

    imported useEffect

--- src/components/Todomodel.js
+++ src/components/Todomodel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import toast from 'react-hot-toast';
 import styles from '../styles/modules/modal.module.scss';
 import { MdOutlineClose } from 'react-icons/md'
