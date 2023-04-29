400f88249c7e7595f3b9e3541008ac951e418813
Davis Kiprotich <mylesdavyz@gmail.com>
2023-04-29 07:57:24 +0300

    Animated modelOpen feature

--- src/components/Todomodel.js
+++ src/components/Todomodel.js
@@ -6,6 +6,28 @@ import Button from './Button';
 import  { addTodo, updateTodo } from '../slices/todoSlice';
 import { useDispatch } from 'react-redux';
 import { v4 as uuid } from 'uuid';
+import { AnimatePresence } from 'framer-motion';
+
+const dropIn = {
+    hidden: {
+        opacity: 0,
+        transform: 'scale(0.9)'
+    },
+    visible: {
+        transform: 'scale(1)',
+        opacity: 1,
+        transition: {
+            duration: 0.1,
+            type: 'spring',
+            damping: 25,
+            stiffness: 500,
+        },
+    },
+    exit: {
+        transform: 'scale(0.9)',
+        opacity: 0,
+    },
+};
 
 function Todomodel({ type, modelOpen, setModelOpen, todo }) {
     const [title, setTitle] = useState('');
@@ -57,71 +79,76 @@ function Todomodel({ type, modelOpen, setModelOpen, todo }) {
                     }));
                 }else{
                     toast.error('No changes made')
+                    return;
                 }
             }
             setModelOpen(false);
         }
     }
   return (
-
-    modelOpen && (
-        <div className={styles.wrapper}>
-            <div className={styles.container}>
-                <div className={styles.closeButton}
-                onClick={() => setModelOpen(false)}
-                onKeyDown={() => setModelOpen(false)}
-                tabIndex={0}
-                role='button'
-                >
-                    <MdOutlineClose/>
+    <AnimatePresence>
+        {modelOpen && (
+            <motion.div className={styles.wrapper} initial={{opacity: 0}} visible={{opacity: 1}} exit={{opacity: 0}}>
+                <div className={styles.container} variants={dropIn} initial='initial' visible='visible' exit='exit'>
+                    <motion.div className={styles.closeButton}
+                    onClick={() => setModelOpen(false)}
+                    onKeyDown={() => setModelOpen(false)}
+                    tabIndex={0}
+                    role='button'
+                    initial={{top: 40, opacity: 0}}
+                    visible={{top: -10, opacity: 1}}
+                    exit={{top: 40, opacity: 0}}
+                    >
+                        <MdOutlineClose/>
+                    </motion.div>
+                    <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+                        <h1 className={styles.formTitle}>{type === 'update' ? 'Update' : 'Add'} Task</h1>
+                        <label htmlFor='title'>
+                            Title
+                            <input type='text' id='title' value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            maxLength='100' />
+                        </label>   
+                        <label htmlFor='description'>
+                            Description
+                            <input type='text' id='description' value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            maxLength='1000' />
+                        </label>   
+                        <label htmlFor='tag'>
+                            Tags
+                            <input type='text' id='tag' value={tag}
+                            onChange={(e) => setTag(e.target.value)} />
+                        </label>   
+                        <label htmlFor='deadline'>
+                            Deadline
+                            <input type='date' id='deadline' value={deadline}
+                            onChange={(e) => setDeadline(e.target.value)}
+                            />
+                        </label>   
+                        <label htmlFor='status'>
+                            Status
+                            <select type='status' id='status' value={status}
+                            onChange={(e) => setStatus(e.target.value)}
+                            >
+                                <option value='open'>OPEN</option>
+                                <option value='working'>WORKING</option>
+                                <option value='done'>DONE</option>
+                                <option value='overdue'>OVERDUE</option>
+                            </select>
+                        </label>  
+                        <div className={styles.buttonContainer}>
+                            <Button type='submit' variant='primary'>{type === 'update' ? 'Update' : 'Add'} Task</Button>
+                            <Button type='button' variant='secondary' 
+                            onClick={() => setModelOpen(false)}
+                            onKeyDown={() => setModelOpen(false)}
+                            >Cancel</Button>
+                        </div> 
+                    </form>
                 </div>
-                <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
-                    <h1 className={styles.formTitle}>{type === 'update' ? 'Update' : 'Add'} Task</h1>
-                    <label htmlFor='title'>
-                        Title
-                        <input type='text' id='title' value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        maxLength='100' />
-                    </label>   
-                    <label htmlFor='description'>
-                        Description
-                        <input type='text' id='description' value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        maxLength='1000' />
-                    </label>   
-                    <label htmlFor='tag'>
-                        Tags
-                        <input type='text' id='tag' value={tag}
-                        onChange={(e) => setTag(e.target.value)} />
-                    </label>   
-                    <label htmlFor='deadline'>
-                        Deadline
-                        <input type='date' id='deadline' value={deadline}
-                        onChange={(e) => setDeadline(e.target.value)}
-                         />
-                    </label>   
-                    <label htmlFor='status'>
-                        Status
-                        <select type='status' id='status' value={status}
-                        onChange={(e) => setStatus(e.target.value)}
-                        >
-                            <option value='open'>OPEN</option>
-                            <option value='working'>WORKING</option>
-                            <option value='done'>DONE</option>
-                            <option value='overdue'>OVERDUE</option>
-                        </select>
-                    </label>  
-                    <div className={styles.buttonContainer}>
-                        <Button type='submit' variant='primary'>{type === 'update' ? 'Update' : 'Add'} Task</Button>
-                        <Button type='button' variant='secondary' 
-                        onClick={() => setModelOpen(false)}
-                        onKeyDown={() => setModelOpen(false)}
-                        >Cancel</Button>
-                    </div> 
-                </form>
-            </div>
-        </div>
-    )    
+            </motion.div>
+        )} 
+    </AnimatePresence>   
     )
 }
 
